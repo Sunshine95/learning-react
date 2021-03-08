39cae170a8e80f14511d5e6c90551603ff479009
Dushyant Goel <dushyant95goel@gmail.com>
2021-03-08 15:47:45 +0530

    Props and Iteration Over Lists Learned

--- yt_custom_search/src/index.js
+++ yt_custom_search/src/index.js
@@ -1,54 +1,51 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
 // stateless functional component
 // always return JSX
 
+const items = [
+  {
+    title: "Selena Gomez",
+    src:
+      "https://1.bp.blogspot.com/-TxsMHjHgQsM/XMvpzRlT3GI/AAAAAAACmt0/I-32IrF9XsIs58fBGq70z7i7ap-0pIYsQCLcBGAs/s1600/Selena-Gomez-Sexy-in-Red-Bra-1.jpg",
+  },
+  {
+    title: "Dua Lipa",
+    src:
+      "https://e00-telva.uecdn.es/assets/multimedia/imagenes/2018/08/01/15331180949718.jpg",
+  },
+  {
+    title: "Camila Cabello",
+    src:
+      "https://i.dailymail.co.uk/i/newpix/2018/06/09/23/4D18162E00000578-0-image-a-187_1528582217475.jpg",
+  },
+  {
+    title: "Demi Lovato",
+    src:
+      "https://media1.popsugar-assets.com/files/thumbor/bU7LcP8p0LDUeuCrbgRJX22pWjs/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/07/19/947/n/44701584/8ba0c7c8e253e9fe_GettyImages-673024878/i/Sexy-Demi-Lovato-Pictures.jpg",
+  },
+];
 
-function Items() {   
-   return ( 
-   <ul>
-    <li><Item/></li>
-    <li><Item/></li>
-    <li><Item/></li>
-    <li><Item/></li>
-   </ul>
-    )
+function ItemList() {
+  return (
+    <div>
+      { items.map((item) => {
+        console.log(item);
+        return <Item item={item}/>
+      })}
+    </div>
+  );
+}
+
+var Item = (props) => {
+  const {title, src} = props.item; 
+  return (
+    <article>
+      <h2>{title}</h2>
+      <img src={src} height="420"></img>;
+    </article>
+  );
 };
 
-function Item() {
-
-  return (
-    <section>
-    <Title></Title>
-    <Image></Image>
-    <Snippet></Snippet>
-    </section>
-  )
-}
-
-function Title() {
-  return (
-    <h4>Selena Gomez</h4>
-  )
-}
-
-function Image(params) {
-  return (
-    <img 
-    src='https://1.bp.blogspot.com/-TxsMHjHgQsM/XMvpzRlT3GI/AAAAAAACmt0/I-32IrF9XsIs58fBGq70z7i7ap-0pIYsQCLcBGAs/s1600/Selena-Gomez-Sexy-in-Red-Bra-1.jpg'
-    alt=''
-    width='500'
-    height=''
-    ></img>
-  )
-}
-
-function Snippet(params) {
-  return (
-    <p> This is Selena Gomez </p>
-  )
-}
-
- ReactDom.render(<Items/>, document.getElementById('root')
- );
+ReactDom.render(<ItemList />, document.getElementById("root"));
