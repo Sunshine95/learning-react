8b8634aca127a5316321d88d6313d2747773962e
Dushyant Goel <dushyant95goel@gmail.com>
2021-03-08 23:01:12 +0530

    useState with object

--- react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Robo",
+    imageURL:
+      "https://cdn.iconscout.com/icon/premium/png-512-thumb/npc-2638654-2185729.png",
+    age: 24,
+    quote: "Hello human, what brings you here?",
+  });
+
+  const handleClick = () => {
+    setPerson({ ...person, quote:'Farewell!'});
+  }
+
+  return (
+    <>
+      <h2>{person.name}</h2>
+      <img src={person.imageURL} height="400"></img>
+      <h4>{person.quote}</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        end conversation
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
