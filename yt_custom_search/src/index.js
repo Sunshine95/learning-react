b75026a1882c750621ff3e14b27de8b214b73de5
Dushyant Goel <dushyant95goel@gmail.com>
2021-03-08 16:57:04 +0530

    basic import-export, breaking up project into files

--- yt_custom_search/src/index.js
+++ yt_custom_search/src/index.js
@@ -4,32 +4,10 @@ import ReactDom from "react-dom";
 // stateless functional component
 // always return JSX
 
-const items = [
-  {
-    id: 1,
-    title: "Selena Gomez",
-    src:
-      "https://1.bp.blogspot.com/-TxsMHjHgQsM/XMvpzRlT3GI/AAAAAAACmt0/I-32IrF9XsIs58fBGq70z7i7ap-0pIYsQCLcBGAs/s1600/Selena-Gomez-Sexy-in-Red-Bra-1.jpg",
-  },
-  {
-    id: 2,
-    title: "Dua Lipa",
-    src:
-      "https://e00-telva.uecdn.es/assets/multimedia/imagenes/2018/08/01/15331180949718.jpg",
-  },
-  {
-    id: 3,
-    title: "Camila Cabello",
-    src:
-      "https://i.dailymail.co.uk/i/newpix/2018/06/09/23/4D18162E00000578-0-image-a-187_1528582217475.jpg",
-  },
-  {
-    id: 4,
-    title: "Demi Lovato",
-    src:
-      "https://media1.popsugar-assets.com/files/thumbor/bU7LcP8p0LDUeuCrbgRJX22pWjs/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/07/19/947/n/44701584/8ba0c7c8e253e9fe_GettyImages-673024878/i/Sexy-Demi-Lovato-Pictures.jpg",
-  },
-];
+// CSS
+// import './index.css';
+
+import {items} from './items';
 
 function ItemList() {
   return (
---
+++ yt_custom_search/src/items.js
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    id: 1,
+    title: "Selena Gomez",
+    src:
+      "https://1.bp.blogspot.com/-TxsMHjHgQsM/XMvpzRlT3GI/AAAAAAACmt0/I-32IrF9XsIs58fBGq70z7i7ap-0pIYsQCLcBGAs/s1600/Selena-Gomez-Sexy-in-Red-Bra-1.jpg",
+  },
+  {
+    id: 2,
+    title: "Dua Lipa",
+    src:
+      "https://e00-telva.uecdn.es/assets/multimedia/imagenes/2018/08/01/15331180949718.jpg",
+  },
+  {
+    id: 3,
+    title: "Camila Cabello",
+    src:
+      "https://i.dailymail.co.uk/i/newpix/2018/06/09/23/4D18162E00000578-0-image-a-187_1528582217475.jpg",
+  },
+  {
+    id: 4,
+    title: "Demi Lovato",
+    src:
+      "https://media1.popsugar-assets.com/files/thumbor/bU7LcP8p0LDUeuCrbgRJX22pWjs/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/07/19/947/n/44701584/8ba0c7c8e253e9fe_GettyImages-673024878/i/Sexy-Demi-Lovato-Pictures.jpg",
+  },
+  {
+    id: 5,
+    title: "Angèle",
+    src:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2d/Festival_des_Vieilles_Charrues_2018_-_Ang%C3%A8le_-_004-2.jpg",
+  },
+  {
+    id: 6,
+    title: "Rita Ora",
+    src:
+      "https://drunkenstepfather.com/wp-content/uploads/2016/02/Rita-Ora-@-Lui-02.2016-1.jpg",
+  },
+];
