90edaf0907b21b6ac9d18e2388354ad117518dcb
Dushyant Goel <dushyant95goel@gmail.com>
2021-03-09 21:16:13 +0530

    completed short-circuit and ternary operator tutorial

--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,30 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [val, setVal] = useState(true)
+
+  return (
+    <>
+      <div class="item">
+        {val ? (
+          <img 
+            src="https://thumb-p8.xhcdn.com/a/91wRd0Be8vMga3nZzHIEvw/000/084/499/448_1000.jpg"
+            alt="true"
+            className="product"
+          ></img>
+        ) : (
+          <img
+            src="https://thumb-p2.xhcdn.com/a/R6M77sA2WUHWbEtRJJPq-Q/000/450/334/182_1000.jpg"
+            alt="false"
+            className="product"
+          ></img>
+        )}
+      </div>
+      <button className="btn" onClick={() => setVal(!val)}>
+        Toggle!
+      </button>
+    </>
+  );
 };
 
 export default ShortCircuit;
