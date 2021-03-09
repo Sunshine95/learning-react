3cdabd5ac5a066f363a080d078797dbefe99ff97
Dushyant Goel <dushyant95goel@gmail.com>
2021-03-09 10:42:59 +0530

    useEffect Basics tutorial

--- react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
+// useEffect is used when we want to change
+// something outside of the component
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]); // re-render everytime any dependency changes
+
+  useEffect(() => {
+    console.log('initial render');
+  }, []) // only runs on intial render
+
+
+  const handleClick = () => {
+    setValue((prevState) => {
+      return prevState + 1;
+    });
+  };
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        +
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
